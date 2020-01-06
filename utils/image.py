c2c5c48385b63a27d478645b9d028e539a411909
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2020-01-06 19:31:54 -0300

    add grayscale support on `capture_picture()`

--- utils/image.py
+++ utils/image.py
@@ -31,20 +31,23 @@ def manual_crop(img, roi):
     return img[int(roi[1]):int(roi[1] + roi[3]), int(roi[0]):int(roi[0] + roi[2])]
 
 
-def auto_crop(img):
+def auto_crop(img, grayscale=False):
     """
     crops image based on bounding rectangle
     """
-    gray = cv2.bitwise_not(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-    roi = cv2.boundingRect(gray)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    mask = cv2.bitwise_not(gray)
+    roi = cv2.boundingRect(mask)
+    if grayscale:
+        return manual_crop(gray, roi)
     return manual_crop(img, roi)
 
 
-def best_crop(img, roi):
-    return auto_crop(manual_crop(img, roi))
+def best_crop(img, roi, grayscale=False):
+    return auto_crop(manual_crop(img, roi), grayscale=grayscale)
 
 
-def capture_picture():
+def capture_picture(grayscale=False):
     try:
         img_grab = grab_image_from_clipboard()
     except (IsADirectoryError, FileNotFoundError, IOError) as e:
@@ -52,6 +55,7 @@ def capture_picture():
 
     image = cv2.cvtColor(img_grab, cv2.COLOR_RGB2BGR)
     roi = cv2.selectROI(WIN_NAME_CROP, image)
+    cv2.destroyWindow(WIN_NAME_CROP)
     if roi[2:] == (0, 0):  # (0, 0, 0, 0) if no ROI is selected; (x, y, 0, 0) if clicked but no area was selected
         return None
-    return best_crop(image, roi)
+    return best_crop(image, roi, grayscale)
