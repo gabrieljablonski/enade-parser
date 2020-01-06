414fc234fcbef1dff634b395c8c29a3a3e760831
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2020-01-06 17:37:31 -0300

    update hotkeys functionality

--- hotkeys.py
+++ hotkeys.py
@@ -1,12 +1,14 @@
 from string import ascii_uppercase
+import keyboard as kb
+from time import sleep
 
 
 class Key:
     CTRL = 'ctrl'
     SPACE = 'space'
+    SHIFT = 'shift'
     ALT = 'alt'
-
-    CTRL_SPACE = f"{CTRL}+{SPACE}"
+    PRNT_SCRN = 'print screen'
 
     def __init__(self):
         for c in ascii_uppercase:
@@ -14,3 +16,33 @@ class Key:
 
 
 Key = Key()
+
+
+def _join_hotkeys(*hotkeys):
+    return '+'.join(hotkeys)
+
+
+def _join_hotkeys_decorator(f):
+    """
+    allows the function to be called as either
+        f(hk1, hk2, ...)
+    or
+        f('hk1+hk2+...')
+    """
+    def new_f(*hotkeys):
+        return f(_join_hotkeys(*hotkeys))
+    return new_f
+
+
+@_join_hotkeys_decorator
+def is_pressed(hotkey):
+    pressed = kb.is_pressed(hotkey)
+    if pressed:
+        kb.release(hotkey)
+    return pressed
+
+
+@_join_hotkeys_decorator
+def send_hotkey(hotkey):
+    kb.press_and_release(hotkey)
+    sleep(.05)
