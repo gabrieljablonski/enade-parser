61f67f87a67025cac37e2b179fa80e4fc8766655
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2020-01-06 18:58:55 -0300

    reorganize hotkey bindings check flow

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import cv2
-from time import time
+from time import time, sleep
 
 from hotkeys import is_pressed, Key
-from xml_tags import TAG_MAPPING, REMOVE_TAG
+from xml_tags import TAG_MAPPING, REMOVE_TAG, NO_PADDING
 from utils.text import modify_selected_text, surround_with, remove_tag
-from utils.image import capture_picture
+from utils.image import capture_picture, WIN_NAME_CAPTURE, WIN_NAME_CROP
 
 
 ACTION_DELAY = 1.
@@ -14,14 +14,59 @@ ACTION_DELAY = 1.
 # QT_X11_NO_MITSHM=1
 
 
+def check_image_operations(image_open, hotkey_pressed):
+    if image_open:
+        pressed = False
+        if is_pressed(Key.ESC):
+            pressed = True
+            print('Cancelling image capture.')
+        elif is_pressed(Key.ENTER):
+            pressed = True
+            print('Saving image.')
+        if pressed:
+            cv2.destroyWindow(WIN_NAME_CAPTURE)
+            image_open = False
+
+    if is_pressed(Key.CTRL, Key.SPACE):
+        hotkey_pressed = True
+        if image_open:
+            print('Finish the operation with the last image first.')
+        else:
+            print("Capturing image.")
+            img = capture_picture()
+            cv2.destroyWindow(WIN_NAME_CROP)
+            if img is None:
+                print('Canceled image capture.')
+            else:
+                cv2.imshow(WIN_NAME_CAPTURE, img)
+                image_open = True
+
+    return image_open, hotkey_pressed
+
+
+def check_tag_operations(hotkey_pressed):
+    for hk, tag in TAG_MAPPING.items():
+        if is_pressed(hk):
+            print(f"Inserting {tag}.")
+            hotkey_pressed = True
+            pad = tag not in NO_PADDING
+            modify_selected_text(surround_with, tag=tag, pad_nl=pad)
+
+    if is_pressed(REMOVE_TAG):
+        print(f"Removed tag.")
+        hotkey_pressed = True
+        modify_selected_text(remove_tag)
+
+    return hotkey_pressed
+
+
 def main():
+    image_open = False
     keyboard_grab_activated = True
     last_action = time()
+
     while True:
-        key = cv2.waitKey(1) & 0xFF
-        if key == 27:
-            cv2.destroyAllWindows()
-            exit()
+        _ = cv2.waitKey(1) & 0xFF
         now = time()
         hotkey_pressed = False
         if now - last_action > ACTION_DELAY:
@@ -31,21 +76,8 @@ def main():
                 print(f"keyboard {'de' if not keyboard_grab_activated else ''}activated")
 
             if keyboard_grab_activated:
-                if is_pressed(Key.SPACE):
-                    print("Capturing image.")
-                    hotkey_pressed = True
-                    cv2.imshow('image', capture_picture())
-
-                for hk, tag in TAG_MAPPING.items():
-                    if is_pressed(hk):
-                        print(f"Inserting {tag}.")
-                        hotkey_pressed = True
-                        modify_selected_text(surround_with, tag=tag)
-
-                if is_pressed(REMOVE_TAG):
-                    print(f"Removed tag.")
-                    hotkey_pressed = True
-                    modify_selected_text(remove_tag)
+                image_open, hotkey_pressed = check_image_operations(image_open, hotkey_pressed)
+                hotkey_pressed = check_tag_operations(hotkey_pressed)
             if hotkey_pressed:
                 last_action = time()
 
