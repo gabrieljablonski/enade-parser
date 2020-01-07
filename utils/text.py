f8759587fba82d1bf38ec538e70222c2cf20bfff
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2020-01-07 10:40:15 -0300

    refactoring

--- utils/text.py
+++ utils/text.py
@@ -6,40 +6,49 @@ from xml_tag_mapping import Tag
 
 
 DEFAULT_INDENT_LEVEL = 4
+CRLF = '\r\n'
+
+
+def sanitize(text):
+    """
+    removes white text around each line from `text`
+    """
+    return CRLF.join(map(str.strip, text.strip().split('\n')))
 
 
 def modify_selected_text(modify, *args, **kwargs):
     send_hotkey(Key.CTRL, Key.C)
     text = paste()
     new_text = modify(text, *args, **kwargs)
-    copy(new_text)
-    send_hotkey(Key.CTRL, Key.V)
+    if text.strip() != new_text:
+        copy(new_text)
+        send_hotkey(Key.CTRL, Key.V)
 
 
-def indent_text(txt, indent_level=DEFAULT_INDENT_LEVEL):
+def indent_text(text, indent_level=DEFAULT_INDENT_LEVEL):
     ind = ' ' * indent_level
-    lines = txt.split('\n')
-    return f"{ind}{ind.join(lines)}"
+    lines = map(str.rstrip, text.split('\n'))
+    return f"{ind}{f'{CRLF}{ind}'.join(lines)}"
 
 
-def dedent_text(txt, indent_level=DEFAULT_INDENT_LEVEL):
+def dedent_text(text, indent_level=DEFAULT_INDENT_LEVEL):
     ind = ' ' * indent_level
     out_lines = []
-    for line in txt.split('\n'):
+    for line in text.split('\n'):
         if line.startswith(ind):
             line = line[len(ind):]
         out_lines.append(line)
     return '\n'.join(out_lines)
 
 
-def surround_with(txt, tag, pad_nl=True, indent_level=DEFAULT_INDENT_LEVEL):
+def surround_with(text, tag, pad_nl=True, indent_level=DEFAULT_INDENT_LEVEL):
     """
         surrounds text with provided tag in XML style
-        surround_with("txt", "tag") -> "<tag>txt</tag>"
+        surround_with("text", "tag") -> "<tag>text</tag>"
     """
-    txt = txt.strip()
+    text = text.strip()
     if tag == Tag.LINK:
-        if not txt.startswith('<') and not txt.endswith('/>'):
+        if not text.startswith('<') and not text.endswith('/>'):
             print('Selected text is invalid. It should be: `<link/>')
             return txt
         txt = txt[1:-2]
@@ -48,18 +57,18 @@ def surround_with(txt, tag, pad_nl=True, indent_level=DEFAULT_INDENT_LEVEL):
     return f"""<{tag}>{padding}{content}{padding}</{tag}>"""
 
 
-def remove_tag(txt):
+def remove_tag(text):
     """
         removes outmost pair of opening and closing tags
-        remove_tag("<tag>txt</tag>") -> "txt"
+        remove_tag("<tag>text</tag>") -> "text"
     """
-    txt = txt.strip()
-    match = re.match(r'^<(\w*)>([\s\S]*)</(\w*)>$', txt)
+    text = text.strip()
+    match = re.match(r'^<(\w*)>([\s\S]*)</(\w*)>$', text)
     if match is None:
-        print(f"No match when removing tags in {repr(txt)}")
-        return txt
+        print(f"No match when trying to remove tags in {repr(text)}")
+        return text
     tag_open, out_text, tag_close = match.groups()
     if tag_open != tag_close:
-        print(f"Opening tag is different from closing tag in {repr(txt)}")
-        return txt
+        print(f"Opening tag is different from closing tag in {repr(text)}")
+        return text
     return dedent_text(out_text).strip()
