802c8f3e2ceb0356bd3de7a729f20674cd8d8a81
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2020-01-08 16:26:00 -0300

    change some tags and add reverse mapping

--- xml_tag_mapping.py
+++ xml_tag_mapping.py
@@ -2,10 +2,10 @@ from utils.hotkeys import Key, join_hotkeys
 
 
 class Tag:
-    HEADER = 'header'  # centered
-    TXT_HEADER = 'text_header'  # left aligned
-    TITLE = 'title'
+    TXT_HEADER = 'text_header'  # 'texto 01', 'texto 02', ...; left aligned
+    TITLE = 'title'  # generic title, centered
     PORQUE = 'porque'
+
     PARAGRAPH = 'paragraph'  # generic paragraph, formatting is not relevant
     TEXT = 'text'  # text that should maintain its formatting (e.g. poems)
     LINK = 'link'
@@ -17,14 +17,17 @@ class Tag:
     QUESTION_BODY = 'question'
     QUESTION_OPTIONS = 'question_options'
     ANSWER_OPTIONS = 'answers'
-    OPTION = 'option'  # for both questions and answers; does not have a hotkey binding
 
     ITALIC = 'i'
     BOLD = 'b'
 
+    # not hotkey bindings
+    FIRST = 'first'  # first sentence inside <porque> tag
+    SECOND = 'second'  # second sentence inside <porque> tag
+    OPTION = 'option'  # for both questions and answers
+
 
 BASE_MAPPING = {
-    Key.Z: Tag.HEADER,
     Key.X: Tag.PORQUE,
     Key.V: Tag.TXT_HEADER,
     Key.A: Tag.PARAGRAPH,
@@ -43,10 +46,14 @@ BASE_MAPPING = {
 }
 
 TAG_HOTKEY_PREFIX = Key.CTRL, Key.ALT
-TAG_MAPPING = {
-    join_hotkeys(*TAG_HOTKEY_PREFIX, k): v
-    for k, v in BASE_MAPPING.items()
-}
-REMOVE_TAG = join_hotkeys(*TAG_HOTKEY_PREFIX, Key.R)
+TAG_MAPPING = {}
+REVERSE_MAPPING = {}
+
+for k, v in BASE_MAPPING.items():
+    hk = join_hotkeys(*TAG_HOTKEY_PREFIX, k)
+    TAG_MAPPING[hk] = v
+    REVERSE_MAPPING[v] = [hk]
+
+HK_REMOVE_TAG = join_hotkeys(*TAG_HOTKEY_PREFIX, Key.R)
 
 NO_PADDING = Tag.ITALIC, Tag.BOLD, Tag.LINK, Tag.FORMULA
