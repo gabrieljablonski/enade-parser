40519cc267802080ba7332712ac7b901876e3f67
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2020-01-08 20:17:23 -0300

    implement interactive menu and save funcionality

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import os
+import re
+import glob
 import cv2
 from time import time, sleep
+from unidecode import unidecode
+from pathlib import Path
+from threading import Thread
+
+from tkinter.filedialog import askopenfilename
+from tkinter import Tk
+Tk().withdraw()
+del Tk
 
 from utils.text import modify_selected_text, surround_with, remove_tag, copy
 from utils.image import capture_image, WIN_NAME_CAPTURE
 from utils.ocr import get_text_in_image
-from utils.hotkeys import is_pressed, Key
+from utils.hotkeys import is_pressed, Key, join_hotkeys
+from utils.static_vars import static_vars
 
 from xml_tag_mapping import TAG_MAPPING, HK_REMOVE_TAG, NO_PADDING
 
+# BadDrawable:
+# sudo nano /etc/environment
+# QT_X11_NO_MITSHM=1
 
 ACTION_DELAY = 1.
 HK_CAPTURE_IMAGE = Key.CTRL, Key.SPACE
@@ -16,11 +31,11 @@ HK_TOGGLE_KEYBOARD = Key.CTRL, Key.SHIFT
 HK_CANCEL = Key.ESC
 HK_CONFIRM = Key.ENTER
 
-# BadDrawable:
-# sudo nano /etc/environment
-# QT_X11_NO_MITSHM=1
+CURRENT_QUESTION_FILE_NAME = 'current_question.xml'
+IMAGES_DIR_NAME = 'images'
 
 
+@static_vars(image=None)
 def check_image_operations(image_open, hotkey_pressed):
     if image_open:
         pressed = False
@@ -28,8 +43,16 @@ def check_image_operations(image_open, hotkey_pressed):
             pressed = True
             print('Cancelling image capture.')
         elif is_pressed(HK_CONFIRM):
-            pressed = True
-            print('Saving image.')
+            try:
+                out_path = save_image(check_image_operations.image)
+
+                tag = f'<img src="{out_path}">'
+                copy(tag)
+                print(f"Copied tag to clipboard: {repr(tag)}")
+
+                pressed = True
+            except Exception as e:
+                print(f"Failed to save image: {e}")
         if pressed:
             cv2.destroyWindow(WIN_NAME_CAPTURE)
             image_open = False
@@ -41,6 +64,7 @@ def check_image_operations(image_open, hotkey_pressed):
         else:
             print('Capturing image.')
             img = capture_image()
+            check_image_operations.image = img
             if img is None:
                 print('Canceled image capture.')
             else:
@@ -85,6 +109,162 @@ def check_ocr_operations(hotkey_pressed):
     return hotkey_pressed
 
 
+def save_image(image):
+    prefix = get_output_file_path_prefix()
+    images = glob.glob(f"{IMAGES_DIR_NAME}/*_{menu.current_question}_*.png")
+
+    if not images:
+        index = 0
+    else:
+        images.sort()
+        last_image = images[-1]
+        match = re.match(r'.*(\d+)\.png$', last_image)
+        index = int(match.group(1)) + 1
+
+    out_path = str(Path(IMAGES_DIR_NAME).joinpath(Path(f"{prefix}_{menu.current_question}_{index}.png")))
+    cv2.imwrite(out_path, image)
+    print(f"Image saved as {out_path}")
+    return out_path
+
+
+def save_question(dst_file_path):
+    if dst_file_path.exists():
+        print(f"File {dst_file_path} already exists. Are you sure you want to override it? (Y/N)")
+        if not prompt_yn():
+            raise OSError(f"File {dst_file_path} already exists")
+        os.remove(dst_file_path)
+    print(f"Saving question as: {dst_file_path}")
+    try:
+        os.rename(CURRENT_QUESTION_FILE_NAME, dst_file_path)
+    except OSError as e:
+        raise OSError(f"failed to rename file {CURRENT_QUESTION_FILE_NAME} to {dst_file_path}: {e}")
+
+
+def prompt_yn():
+    while True:
+        opt = input('>> ')
+        if opt.lower() in ('y', 'n'):
+            return opt == 'y'
+
+
+def get_subject_year():
+    while True:
+        print('Type out the subject and year in the format: <subject>, <year>')
+        inp = input('>> ').split(',')
+        inp = list(map(str.strip, inp))
+        if len(inp) != 2 or not inp[0] or not inp[1]:
+            print('Invalid format.')
+            continue
+        subject, year = inp
+        print(
+            f"-Subject: {subject}\n"
+            f"-Year: {year}\n"
+            f"Is this correct? (Y/N)"
+        )
+        if prompt_yn():
+            return subject, year
+
+
+def open_current_question_file():
+    open(CURRENT_QUESTION_FILE_NAME, 'a').close()
+    os.system(CURRENT_QUESTION_FILE_NAME)
+
+
+def get_output_file_path_prefix():
+    s = '_'.join(filter(str.strip, menu.subject.replace('-', '').split()))
+    return f"{unidecode(s.lower())}_{menu.year}"
+
+
+@static_vars(subject='', year='', current_question=1)
+def menu():
+    while True:
+        if not menu.subject:
+            print('Select the file to be analyzed.')
+            file_name = askopenfilename()
+            manual = True
+            if file_name:
+                subject, year = map(str.strip, os.path.split(file_name)[1].split('_')[0:2])
+                print(
+                    f"Info from file `{file_name}`\n"
+                    f"-Subject: {subject}\n"
+                    f"-Year: {year}\n"
+                    f"Is this correct? (Y/N)"
+                )
+                manual = not prompt_yn()
+            if manual:
+                subject, year = get_subject_year()
+
+            menu.subject, menu.year = subject, year
+            base_path = f"{subject} {year}"
+            Path(base_path).joinpath(Path(IMAGES_DIR_NAME)).mkdir(parents=True, exist_ok=True)
+            os.chdir(base_path)
+        else:
+            options = {
+                1: 'Enable keyboard grab.',
+                2: f"Change current question number (current is {menu.current_question}).",
+                3: 'Change subject/year.',
+                4: 'Save current question.',
+                0: 'Exit.'
+            }
+            print('Choose an option:')
+            for o, txt in options.items():
+                print(f"-{o}: {txt}")
+            while True:
+                inp = input('>> ')
+                try:
+                    opt = int(inp)
+                    if opt not in options:
+                        raise ValueError
+                    break
+                except ValueError:
+                    print('Invalid option')
+
+            if opt == 0:
+                exit()
+
+            if opt == 1:
+                print(f"Enabling keyboard grab, use {join_hotkeys(HK_TOGGLE_KEYBOARD).upper()} to disable it.")
+                open_current_question_file()
+                return
+
+            if opt == 2:
+                print('Insert new question number:')
+                while True:
+                    inp = input('>> ')
+                    try:
+                        question_number = int(inp)
+                        if question_number < 1:
+                            raise ValueError
+                        menu.current_question = question_number
+                        break
+                    except ValueError:
+                        print('Invalid question number.')
+
+            if opt == 3:
+                os.chdir('..')
+                menu.subject, menu.year, menu.active_dir, menu.images_dir, menu.current_question = '', '', '', '', 1
+                continue
+
+            if opt == 4:
+                print('Is the question discursive or multiple choice? (M/d)')
+                while True:
+                    inp = input('>> ')
+                    if not inp or inp.lower() in ('d', 'm'):
+                        question_type = inp.lower() or 'm'
+                        break
+
+                prefix = get_output_file_path_prefix()
+                qt = 'choice' if question_type == 'm' else 'disc'
+                out_file_path = Path(f"{prefix}_{menu.current_question}_{qt}.xml")
+
+                try:
+                    save_question(out_file_path)
+                except OSError as e:
+                    print(e)
+                else:
+                    menu.current_question += 1
+                finally:
+                    continue
 
 
 def main():
@@ -95,12 +275,16 @@ def main():
     while True:
         _ = cv2.waitKey(1) & 0xFF
         now = time()
+        if not keyboard_grab_activated:
+            menu()
+            keyboard_grab_activated = True
+
         hotkey_pressed = False
         if now - last_action > ACTION_DELAY:
             if is_pressed(HK_TOGGLE_KEYBOARD):
                 hotkey_pressed = True
                 keyboard_grab_activated = False
-                print(f"keyboard {'de' if not keyboard_grab_activated else ''}activated")
+                print(f"Keyboard deactivated")
 
             if keyboard_grab_activated:
                 image_open, hotkey_pressed = check_image_operations(image_open, hotkey_pressed)
