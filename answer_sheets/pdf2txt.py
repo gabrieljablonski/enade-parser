36d12ab7d88f3d37b5c8064eb6c4b076e7b78cb0
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2020-03-13 23:32:37 -0300

    a

---
+++ answer_sheets/pdf2txt.py
@@ -0,0 +1,23 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import BytesIO
+
+def pdf_to_text(path):
+    manager = PDFResourceManager()
+    retstr = BytesIO()
+    layout = LAParams(all_texts=True)
+    device = TextConverter(manager, retstr, laparams=layout)
+    filepath = open(path, 'rb')
+    interpreter = PDFPageInterpreter(manager, device)
+
+    for page in PDFPage.get_pages(filepath, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    filepath.close()
+    device.close()
+    retstr.close()
+    return text
